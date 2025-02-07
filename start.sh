985f110756705bc2a8bc2d5390e816757320b99c
Mike <presidentComp@gmail.com>
2025-02-07 21:48:58 +0300

    Изменения, которые будут включены в коммит: 	изменено:      NetScan/NetScan.py 	новый файл:    sf_test 	изменено:      start.sh

--- start.sh
+++ start.sh
@@ -26,18 +26,42 @@ sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
+echo -e "\033[32m Установка Apache2 завершена.\033[0m"
+
 # Установка Python
 sudo apt-get install -y python
+python3 --version
+
+echo -e "\033[32m Установка Python завершена.\033[0m"
 
 # Установка и поднятие SSH-сервера
-sudo apt-get install -y openssh-server
+sudo apt-get install ssh
 sudo systemctl start ssh
 sudo systemctl enable ssh
+
+echo -e "\033[32m Установка SSH завершена.\033[0m"
+
+# Включение PubkeyAuthentication в sshd_config
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
 # Установка Docker
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
+
+echo -e "\033[32m Установка Docker завершена.\033[0m"
+
+# Установка MySQL-сервера
+sudo apt install mysql-server
+
+# Проверка версии MySQL
+mysql -V
+
+# Автоматический запуск MySQL при загрузке системы
+sudo systemctl start mysql
+sudo systemctl enable mysql
+
+echo -e "\033[32m Установка MySQL завершена.\033[0m"
 
 # Установка conky
 sudo apt-get install -y conky
@@ -48,7 +72,19 @@ sudo apt-get install -y zenmap
 # Установка Portmaster (мониторинг портов)
 sudo apt-get install -y portmaster
 
+echo -e "\033[32m Установка Portmaster завершена.\033[0m"
+
 # Установка Wireshark
 sudo apt-get install -y wireshark
 
-echo "Настройка системы завершена."
+echo -e "\033[32m Установка Wereshark завершена.\033[0m"
+
+# Установка FTP
+sudo apt-get install vsftpd
+
+# Добавление строк в /etc/vsftpd.conf
+sudo sed -i '\$a\ anonymous_enable = NO\n local_enable = YES\n write_enable = YES\n local_umask = 022\n xferlog_enable = YES\n xferlog_std_format=YES\n connect_from_port_20 = YES\n chroot_local_user = YES\n allow_writeable_chroot = YES\n ssl_enable=YES\n ssl_tlsv1=YES\n ssl_sslv2=NO\n ssl_sslv3=NO\n rsa_cert_file=/etc/ssl/private/ssl-cert-snakeoil.pem\n rsa_private_key_file=/etc/sssl_ciphers=HIGH/private/ssl-cert-snakeoil.key\n allow_anon_ssl=NO\n force_local_data_ssl=YES\n force_local_logins_ssl=YES\n ssl_ciphers=HIGH\n use_localtime=YES' /etc/vsftpd.conf
+
+echo -e "\033[32m Установка и настройка FTP завершены.\033[0m"
+
+echo -e "\033[32m Настройка системы завершена.\033[0m"
